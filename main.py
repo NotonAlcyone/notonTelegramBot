c6b3297db094ab6a6cd1a0f226f8b3ff3e9954f4
Lee Dongyun <notonalcyone@gmail.com>
2019-03-02 20:43:36 +0900

    pep8 스타일 가이드 준수

--- main.py
+++ main.py
@@ -33,195 +33,240 @@ def get_message(bot, update) : #echo
 def getAdmin(bot,update): # admin answer
 	update.message.reply_text("adminSay")
 """
-def dbInit():
+
+
+def db_init():
 	con = sqlite3.connect("player.db")
 	cursor = con.cursor()
-	cursor.execute('CREATE TABLE IF NOT EXISTS chatMorningCallList("callIndex" INTEGER NOT NULL PRIMARY KEY ,"chatNumber"INTEGER NOT NULL,"morningCallTime"TEXT,"morningCallText"TEXT)')
+	cursor.execute(
+		'CREATE TABLE IF NOT EXISTS chatMorningCallList'
+		'("callIndex" INTEGER NOT NULL PRIMARY KEY ,"chatNumber"INTEGER NOT NULL,"morningCallTime"TEXT,"morningCallText"TEXT)'
+	)
 	cursor.execute('CREATE TABLE IF NOT EXISTS chatUTCData("chatID" INTEGER NOT NULL PRIMARY KEY, "utcData" INTEGER)')
 	con.close()
 
-def helpCMD(bot, update):  # /help 명령어 입력시 작동되는 함수
+
+def cmd_help(bot, update):  # /help 명령어 입력시 작동되는 함수
 	update.message.reply_text("Private Bot System For NotonAlcyone")
 	print(update.message.from_user.first_name)
-	logDB(str(update.message.text), "Return", update.message.from_user.id)
+	db_logger(str(update.message.text), "Return", update.message.from_user.id)
 
-def diceCMD(bot, update):  # /dice 명령어 입력시 작동되는 함수
-	randNum = random.randrange(1, 999)  # 1~999까지 랜덤 작동
+
+def cmd_dice(bot, update):  # /dice 명령어 입력시 작동되는 함수
+	rand_num = random.randrange(1, 999)  # 1~999까지 랜덤 작동
 	print(update.message.from_user.first_name)
-	if randNum % 10 in [0, 1, 3, 6, 7, 8]: # 랜덤값의 마지막 자리에 따라서 조사 이,가 를 결정해줌
-		postPostion = "이"
+	if rand_num % 10 in [0, 1, 3, 6, 7, 8]:  # 랜덤값의 마지막 자리에 따라서 조사 이,가 를 결정해줌
+		post_position = "이"
 	else:
-		postPostion = "가"
-	bot.send_message(update.message.chat_id, update.message.from_user.first_name + "님께서 주사위를 굴려 🎲" + str(randNum)+ postPostion + " 나왔습니다")
-	logDB(str(update.message.text), str(randNum), update.message.from_user.id)  # 로그 저장
+		post_position = "가"
+	bot.send_message(
+		update.message.chat_id,
+		update.message.from_user.first_name + "님께서 주사위를 굴려 🎲" + str(rand_num) + post_position + " 나왔습니다"
+	)
+	db_logger(str(update.message.text), str(rand_num), update.message.from_user.id)  # 로그 저장
 
-def selectCMD(bot, update): # /select 명령어 입력시 작동되는 함수
-	afterData = update.message.text.split()  # 입력된 데이터 분리
-	if len(afterData) == 1:  # argument 입력이 없는 경우
-		bot.send_message(update.message.chat_id,"wrongMessage Input")
-		logDB(str(update.message.text),"Select Fail",update.message.from_user.id)
 
-	else: # argument 입력이 있는 경우 해당 중 1개 선택
-		tmpData = afterData[random.randrange(1,len(afterData))]
-		bot.send_message(update.message.chat_id,tmpData)
-		logDB(str(update.message.text), tmpData, update.message.from_user.id)
+def cmd_select(bot, update):  # /select 명령어 입력시 작동되는 함수
+	input_text = update.message.text.split()  # 입력된 데이터 분리
+	if len(input_text) == 1:  # argument 입력이 없는 경우
+		bot.send_message(update.message.chat_id, "잘못된 메시지 입력입니다.")
+		db_logger(str(update.message.text), "Select Fail", update.message.from_user.id)
 
-def logCMD (bot, update):  # /log 명렁어 입력시 작동되는 함수
-	if update.message.from_user.id in adminID: # 입력자가 admin일 경우에만 출력
+	else:  # argument 입력이 있는 경우 해당 중 1개 선택
+		chosen_data = input_text[random.randrange(1, len(input_text))]
+		bot.send_message(update.message.chat_id, chosen_data)
+		db_logger(str(update.message.text), chosen_data, update.message.from_user.id)
+
+
+def cmd_log(bot, update):  # /log 명렁어 입력시 작동되는 함수
+	if update.message.from_user.id in adminID:  # 입력자가 어드민일 경우에만 출력
 		conn = sqlite3.connect("log.db")
 		cursor = conn.cursor()	
-		cursor.execute("SELECT * FROM commandLog ORDER BY commandServerTime DESC Limit 10") #로그 기록 시간 기준으로 내림차순 정렬해서 10개를 출력
-		logData = cursor.fetchall()
-		logData.reverse() # 가장 최근데이터가 상위에 있으므로, 보기 편하게 리버스
-		lineBreakData = ""
-		for i in range(0,10):
-			lineBreakData += str(logData[i]) + " \n"
-		bot.send_message(update.message.chat_id,lineBreakData)
-		logDB(str(update.message.text),"recentLog * 10",update.message.from_user.id)
-	else: #요청자가 어드민이 아닐 경우 출력
-		bot.send_message(update.message.chat_id,"You are not admin")
-		logDB(str(update.message.text),"log Request rejected",update.message.from_user.id)
+		cursor.execute("SELECT * FROM commandLog ORDER BY commandServerTime DESC Limit 10")
+		# 로그 기록 시간 기준으로 내림차순 정렬해서 10개를 출력
+		log_data = cursor.fetchall()
+		log_data.reverse()  # 가장 최근데이터가 상위에 있으므로, 보기 편하게 리버스
+		line_break_data = ""
+		for i in range(0, 10):
+			line_break_data += str(log_data[i]) + " \n"
+		bot.send_message(update.message.chat_id, line_break_data)
+		db_logger(str(update.message.text), "recentLog * 10", update.message.from_user.id)
+	else:  # 요청자가 어드민이 아닐 경우 출력
+		bot.send_message(update.message.chat_id, "You are not admin")
+		db_logger(str(update.message.text), "log Request rejected", update.message.from_user.id)
 	
 
-def weatherCMD(bot, update):
+def cmd_weather(bot, update):
 	try:
-		weatherAnswer = weatherData(False)#weatherData에 데이터 요청
-		bot.send_message(update.message.chat_id,"현재 서울 기온은 "+weatherAnswer[0] +"℃ 입니다.\n"+"미세먼지: "+weatherAnswer[1]+" 입니다."+"\n초미세먼지: "+weatherAnswer[2]+" 입니다.")
-		logDB(str(update.message.text),"날씨 데이터 조회 ",update.message.from_user.id)
+		weatherAnswer = cash_weather(False)  # weatherData에 데이터 요청
+		bot.send_message(
+			update.message.chat_id,
+			"현재 서울 기온은 " + weatherAnswer[0] + "℃ 입니다.\n" +
+			"미세먼지: " + weatherAnswer[1] + " 입니다." +
+			"\n초미세먼지: " + weatherAnswer[2] + " 입니다."
+		)
+		db_logger(str(update.message.text), "날씨 데이터 조회 ", update.message.from_user.id)
 	except:
-	#파싱중 오류 발생시 출력
-		bot.send_message(update.message.chat_id,"날씨 데이터 조회 중 에러 발생")
-		logDB(str(update.message.text),"날씨 데이터 조회 실패",update.message.from_user.id)
+		# 파싱중 오류 발생시 출력
+		bot.send_message(update.message.chat_id, "날씨 데이터 조회 중 에러 발생")
+		db_logger(str(update.message.text), "날씨 데이터 조회 실패", update.message.from_user.id)
 
-def logDB(command,answer,commandUser):
+
+def db_logger(command, answer, commandUser):
 	
 	conn = sqlite3.connect("log.db")
 	cursor = conn.cursor()
-	cursor.execute('CREATE TABLE IF NOT EXISTS commandLog("commandType"TEXT NOT NULL,"commandServerTime"INTEGER NOT NULL,"commandAnswer"TEXT, "commandUser"INTEGER NOT NULL)') #해당DB가 없을경우 삭제
-	#print(cursor.fetchall())
+	cursor.execute(
+		'CREATE TABLE IF NOT EXISTS commandLog'
+		'("commandType"TEXT NOT NULL,"commandServerTime"INTEGER NOT NULL,"commandAnswer"TEXT, "commandUser"INTEGER NOT NULL)'
+	) # 해당 DB가 없을경우 삭제
+	# print(cursor.fetchall())
 	print("-------------------")
 	print(command)
 	print(answer)
 	
-	logTmp = (command, int(time.time()),answer,commandUser)
-	cursor.execute("INSERT INTO commandLog VALUES(?,?,?,?)",logTmp)
-	#print("dose is on")
+	input_log = (command, int(time.time()), answer, commandUser)
+	cursor.execute("INSERT INTO commandLog VALUES(?,?,?,?)", input_log)
 	conn.commit()
 	conn.close()
 
-def weatherData(isInit):
+
+def cash_weather(is_init):
 	global tempData
 	global dustData
-	if isInit == False:
+	if is_init is False:
 		if tempData == "" or dustData == "": # 초기화 콜이 아니고, 데이터가 비어있을때 파싱
-			tmp = weatherParser()
+			tmp = parser_weather()
 			tempData = tmp[0]
 			dustData = tmp[1]
-			threading.Timer(dataCashingTime, weatherData,[True]).start() #캐싱 타임 이후 캐싱데이터 초기화
-			return(tempData[0].text,dustData[0].text,dustData[1].text)
+			threading.Timer(dataCashingTime, cash_weather, [True]).start()  # 캐싱 타임 이후 캐싱데이터 초기화
+			return tempData[0].text, dustData[0].text, dustData[1].text
 
 		else:
-			return(tempData[0].text,dustData[0].text,dustData[1].text)
+			return tempData[0].text, dustData[0].text, dustData[1].text
 
-	else: #캐싱 데이터 초기화
+	else:  # 캐싱 데이터 초기화
 		tempData = ""
 		dustData = ""
 
-def weatherParser():#네이버 날씨 파싱
-	naverWeatherResponse = requests.get(naverWeatherURL)
-	naverWeatherData = BeautifulSoup(naverWeatherResponse.text,'html.parser')
-	tempData = naverWeatherData.select("span.todaytemp")
-	dustData = naverWeatherData.select("dl.indicator dd")
-	return(tempData,dustData)
+
+def parser_weather():  # 네이버 날씨 파싱
+	naver_weather_response = requests.get(naverWeatherURL)
+	naver_weather_data = BeautifulSoup(naver_weather_response.text,'html.parser')
+	temp_data = naver_weather_data.select("span.todaytemp")
+	dust_data = naver_weather_data.select("dl.indicator dd")
+	return temp_data, dust_data
 
 
-def getDBData(isWhere,dbName,query,insertData = None):
-	con = sqlite3.connect(dbName)
+def db_get_data(is_where, db_name, query, data_insert=None):
+	con = sqlite3.connect(db_name)
 	cursor = con.cursor()
-	if isWhere == True:
-		cursor.execute(query,insertData)
-		returnData = cursor.fetchall()
+	if is_where is True:
+		cursor.execute(query, data_insert)
+		return_data = cursor.fetchall()
 	else:
 		cursor.execute(query)
-		returnData = cursor.fetchall()
+		return_data = cursor.fetchall()
 	con.close()
-	return returnData
+	return return_data
 
-def insertDBData(dbName, query, insertData):
-	con = sqlite3.connect(dbName)
+
+def db_edit_data(db_name, query, data_insert):
+	con = sqlite3.connect(db_name)
 	cursor = con.cursor()
-	cursor.execute(query, insertData)
+	cursor.execute(query, data_insert)
 	con.commit()
 	con.close()
 
-def addBotCallCMD(bot,update):
-	try:
-		callData = update.message.text.split()
-		callTime = datetime.datetime.strptime(callData[1], '%H:%M') #1차 변환 시도
-		callText = ""
-		for i in range(2, len(callData)):
-			callText += str(callData[i]) + " "
-		insertDBData("player.db", "INSERT INTO chatMorningCallList  VALUES(?,?,?,?)", (update.update_id, update.message.chat.id, callData[1], callText))
-		insertDBData("player.db", "INSERT OR IGNORE INTO chatUTCData VALUES(?,?)", (update.message.chat.id, 0))  # UTC값 db 전송
-		# print(getDBData(False,"player.db","SELECT callIndex FROM chatMorningCallList order by callIndex desc limit 1")[0][0])
-		addJob(update.update_id)
 
-		bot.send_message(update.message.chat_id,"정상적으로 Call이 등록되었습니다.")
-		logDB(str(update.message.text),"Call 등록 성공",update.message.from_user.id)
+def cmd_add_daily_call(bot, update):
+	try:
+		input_text = update.message.text.split()
+		input_time = datetime.datetime.strptime(input_text[1], '%H:%M') # 1차 변환 시도
+		call_text = ""
+		for i in range(2, len(input_text)):
+			call_text += str(input_text[i]) + " "
+		db_edit_data(
+			"player.db", "INSERT INTO chatMorningCallList  VALUES(?,?,?,?)",
+			(update.update_id, update.message.chat.id, input_text[1], call_text)
+		)
+		db_edit_data("player.db", "INSERT OR IGNORE INTO chatUTCData VALUES(?,?)", (update.message.chat.id, 0))  # UTC db 전송
+		# print(
+		# getDBData(False,"player.db","SELECT callIndex FROM chatMorningCallList order by callIndex desc limit 1")[0][0]
+		# )
+		job_add(update.update_id)
+
+		bot.send_message(update.message.chat_id, "정상적으로 Call이 등록되었습니다.")
+		db_logger(str(update.message.text), "Call 등록 성공", update.message.from_user.id)
 	except:
-		bot.send_message(update.message.chat_id,"Call 등록 실패")
-		logDB(str(update.message.text),"Call 등록 실패",update.message.from_user.id)
+		bot.send_message(update.message.chat_id, "Call 등록 실패")
+		db_logger(str(update.message.text), "Call 등록 실패", update.message.from_user.id)
 
-def addJob(jobDBIndex):
+
+def job_add(job_index):
 	global jobQueue
-	insertData = (jobDBIndex,)
-	DBdata = getDBData(True,"player.db", "SELECT chatNumber, morningCallTime  FROM chatMorningCallList WHERE callIndex = ? ",insertData)
-	chatNumber = (DBdata[0][0],)
-	morningCallTime = datetime.datetime.strptime(DBdata[0][1], '%H:%M')
-	print(morningCallTime)
-	chatUTC = getDBData(True,"player.db","SELECT utcData FROM chatUTCData where chatID = ?",chatNumber)
-	messageTime = morningCallTime - datetime.timedelta(hours= chatUTC[0][0])
+	insert_data = (job_index,)
+	db_data = db_get_data(
+		True,
+		"player.db", "SELECT chatNumber, morningCallTime  FROM chatMorningCallList WHERE callIndex = ? ",
+		insert_data
+	)
+	chat_number = (db_data[0][0],)
+	call_time = datetime.datetime.strptime(db_data[0][1], '%H:%M')
+	print(call_time)
+	chat_utc = db_get_data(True, "player.db", "SELECT utcData FROM chatUTCData where chatID = ?", chat_number)
+	message_time = call_time - datetime.timedelta(hours= chat_utc[0][0])
 
-	print(messageTime.time())
-	jobQueue = updater.job_queue.run_daily(exeJob,time = messageTime.time() ,name = str(jobDBIndex))
+	print(message_time.time())
+	jobQueue = updater.job_queue.run_daily(job_exe, time=message_time.time(), name=str(job_index))
 
-def delete_job(jobname, isHardDel):
+
+def delete_job(job_name, is_hard_delete):
 	global jobQueue
 	try:
-		if isHardDel == True:
-			insertDBData("player.db","DELETE FROM chatMorningCallList where callIndex = ?",(jobname,))
-		jobQueue.job_queue.get_jobs_by_name(str(jobname))[0].schedule_removal()
+		if is_hard_delete is True:
+			db_edit_data("player.db", "DELETE FROM chatMorningCallList where callIndex = ?", (job_name,))
+		jobQueue.job_queue.get_jobs_by_name(str(job_name))[0].schedule_removal()
 	except:
 		print("삭제 실패")
-		print(jobname)
+		print(job_name)
 
-def exeJob(bot, job):
-	insertData = (job.name,)
-	callData = getDBData(True,"player.db", "SELECT chatNumber, morningCallText FROM chatMorningCallList WHERE callIndex = ? ",insertData)
-	callText = callData[0][1]
-	callID = callData[0][0]
+
+def job_exe(bot, job):
+	insert_data = (job.name,)
+	call_data = db_get_data(True, "player.db", "SELECT chatNumber,morningCallText FROM chatMorningCallList WHERE callIndex = ? ", insert_data)
+	call_text = call_data[0][1]
+	call_id = call_data[0][0]
 	print("job call")
 	print(job.name)
-	bot.send_message(callID, callText)
+	bot.send_message(call_id, call_text)
 
-def cmd_del_call(bot,update):
-	delList = getDBData(True,"player.db","SELECT callIndex From chatMorningCallList where chatNumber = ?",(update.message.chat_id,))
-	for i in range(0,len(delList)):
-		delete_job(delList[i][0],True)
+
+def cmd_del_call(bot, update):
+	del_list = db_get_data(
+		True,
+		"player.db", "SELECT callIndex From chatMorningCallList where chatNumber = ?",
+		(update.message.chat_id,)
+	)
+	for i in range(0, len(del_list)):
+		delete_job(del_list[i][0], True)
 
 
 def cmd_set_utc(bot, update):
-	utcSetData = update.message.text.split()
-	print(utcSetData[1])
+	utc_set_data = update.message.text.split()
+	print(utc_set_data[1])
 	try:
-		if int(utcSetData[1]) in range(-12,14):
-			utcUpdate = (update.message.chat.id,utcSetData[1])
-			insertDBData("player.db","INSERT OR REPLACE INTO chatUTCData VALUES(?,?)",utcUpdate)
-			#delList = getDBData(True,"player.db","SELECT callIndex From chatMorningCallList where chatNumber = ?",(update.message.chat_id,))
-			#for i in range(0,len(delList)):
-			#	delete_job(delList[i][0],False)
-			#for i in range(0,len(delList)):
-			#	addJob(delList[i][0])
+		if int(utc_set_data[1]) in range(-12,14):
+			utc_update = (update.message.chat.id,utc_set_data[1])
+			db_edit_data("player.db", "INSERT OR REPLACE INTO chatUTCData VALUES(?,?)", utc_update)
+			# delList = getDBData(
+			# True,"player.db","SELECT callIndex From chatMorningCallList where chatNumber = ?",
+			# (update.message.chat_id,)
+			# )
+			# for i in range(0,len(delList)):
+			# delete_job(delList[i][0],False)
+			# for i in range(0,len(delList)):
+			# addJob(delList[i][0])
 
 		else:
 			bot.send_message(update.message.chat_id,"UTC 범위(-12~14) 의 숫자가 아닙니다.")
@@ -229,8 +274,7 @@ def cmd_set_utc(bot, update):
 		bot.send_message(update.message.chat_id,"잘못된 입력입니다.")
 
 
-
-# call 목록 불러오기 
+# call 목록 불러오기
 # call 삭제 (DB 삭제 + 현재 액티브 삭제)
 # UTC 세팅
 # 콜 이니셜라이저
@@ -248,29 +292,30 @@ def test(bot, job):
 
 	print("jobjob")
 """
-#def addMorningCall():
+# def addMorningCall():
 
-#def deleteMorningCall():
+# def deleteMorningCall():
 
-#def loadMorningCall(isAll):
-	#시작할때 전체 로드랑, ADD모닝콜에서 왔을때 맨 마지막거 로드
+# def loadMorningCall(isAll):
+# 시작할때 전체 로드랑, ADD 모닝콜에서 왔을때 맨 마지막거 로드
 
-#j = updater.job_queue.run_daily(callBackDaily, time = datetime.datetime.strptime('23:00', '%H:%M').time() ) #서버시간 UTC임 아아아아아아ㅏ악
-#j = updater.job_queue.run_repeating(callBackDaily, interval = 3, name = "15151")
-#j = updater.job_queue.run_repeating(test, interval = 3, name = "131313")
+# j = updater.job_queue.run_daily(callBackDaily, time = datetime.datetime.strptime('23:00', '%H:%M').time() )
+# 서버시간 UTC 임 아아아아아아ㅏ악
+# j = updater.job_queue.run_repeating(callBackDaily, interval = 3, name = "15151")
+# j = updater.job_queue.run_repeating(test, interval = 3, name = "131313")
 
 
-#updater.dispatcher.add_handler(MessageHandler(Filters.text, get_message))
-#updater.dispatcher.add_handler(MessageHandler(Filters.chat(adminID[0]), getAdmin))
-dbInit()
-cmdHelp = CommandHandler(["help","HELP"],helpCMD)
-cmdDice = CommandHandler(["dice","DICE"],diceCMD)
-cmdSelect = CommandHandler(["select","Select"],selectCMD)
-cmdWeather = CommandHandler(["weather","Weather"],weatherCMD)
-cmdLog = CommandHandler(["log","LOG"],logCMD)
-cmdAddCall = CommandHandler("addCall",addBotCallCMD)
-cmdSetUtc = CommandHandler("setUTC",cmd_set_utc)
-cmdDelJob = CommandHandler("delCall",cmd_del_call)
+# updater.dispatcher.add_handler(MessageHandler(Filters.text, get_message))
+# updater.dispatcher.add_handler(MessageHandler(Filters.chat(adminID[0]), getAdmin))
+db_init()
+cmdHelp = CommandHandler(["help", "HELP"], cmd_help)
+cmdDice = CommandHandler(["dice", "DICE"], cmd_dice)
+cmdSelect = CommandHandler(["select", "Select"], cmd_select)
+cmdWeather = CommandHandler(["weather", "Weather"], cmd_weather)
+cmdLog = CommandHandler(["log", "LOG"], cmd_log)
+cmdAddCall = CommandHandler("addCall", cmd_add_daily_call)
+cmdSetUtc = CommandHandler("setUTC", cmd_set_utc)
+cmdDelJob = CommandHandler("delCall", cmd_del_call)
 updater.dispatcher.add_handler(cmdHelp)
 updater.dispatcher.add_handler(cmdDice)
 updater.dispatcher.add_handler(cmdSelect)
